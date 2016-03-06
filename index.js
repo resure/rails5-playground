022b1360bf0766d0056c67ac00083e84da2d30f0
Gadzhi Gadzhiev <inbox@resure.net>
2016-03-07 00:15:31 +0300

    Remove CLI for now & integrate nodemon

--- index.js
+++ index.js
@@ -1,61 +1,74 @@
 'use strict';
 
-module.exports = function (opts) {
-	if (opts.debug) {
-		process.env.DEBUG = 'server';
+const path = require('path');
+const app = require(path.join(__dirname, 'app.js'));
+const debug = require('debug')('server');
+const http = require('http');
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(app.get('port'));
+server.on('error', onError);
+server.on('listening', onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
 	}
 
-	const path = require('path');
-	const app = require(path.join(__dirname, 'app.js'));
-	const debug = require('debug')('server');
-	const http = require('http');
+	if (port >= 0) {
+		// port number
+		return port;
+	}
 
-	app.set('port', opts.port);
+	return false;
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
 
-	const server = http.createServer(app);
-	server.listen(app.get('port'));
-	server.on('error', onError);
-	server.on('listening', onListening);
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+	const port = app.get('port');
 
-	/**
-	 * Event listener for HTTP server "error" event.
-	 */
+	const bind = typeof port === 'string' ?
+		'Pipe ' + port :
+		'Port ' + port;
 
-	function onError(error) {
-		if (error.syscall !== 'listen') {
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
 			throw error;
-		}
-		const port = app.get('port');
-
-		const bind = typeof port === 'string' ?
-			'Pipe ' + port :
-			'Port ' + port;
-
-		// handle specific listen errors with friendly messages
-		switch (error.code) {
-			case 'EACCES':
-				console.error(bind + ' requires elevated privileges');
-				process.exit(1);
-				break;
-			case 'EADDRINUSE':
-				console.error(bind + ' is already in use');
-				process.exit(1);
-				break;
-			default:
-				throw error;
-		}
 	}
+}
 
-	/**
-	 * Event listener for HTTP server "listening" event.
-	 */
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
-	function onListening() {
-		var addr = server.address();
-		var bind = typeof addr === 'string' ?
-			'pipe ' + addr :
-			'port ' + addr.port;
-		debug('Listening on ' + bind);
-	}
-};
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === 'string' ?
+		'pipe ' + addr :
+		'port ' + addr.port;
+	debug('Listening on ' + bind);
+}
 
