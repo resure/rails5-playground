b775bb802b58abea91f2d9a2b739e91dd39a8178
Gadzhi Gadzhiev <inbox@resure.net>
2016-03-08 20:48:51 +0300

    Add logster

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
+  # constraints lambda { |req| req.session['admin'] } do
+  #   mount Logster::Web => '/logs'
+  # end
+  mount Logster::Web => '/logs'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
