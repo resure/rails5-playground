932d09679b0d09e597d4edea98982dcefe29abd2
Gadzhi Gadzhiev <inbox@resure.net>
2016-03-08 23:26:34 +0300

    Basic user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def ensure_auth
+    authenticate_with_http_basic do |username, password|
+      user = User.find_by_username('username')
+      user and user.authenticate password
+    end
+  end
 end
